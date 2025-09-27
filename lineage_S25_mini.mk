65eb55c09034e06838977d7c1e751618a49f534d
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-09-27 17:45:52 +0100

    Fixup some issues

--- lineage_S25_mini.mk
+++ lineage_S25_mini.mk
@@ -6,14 +6,30 @@
 #
 
 # Inherit from those products. Most specific first.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+include $(SRC_TARGET_DIR)/product/treble_common.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit proprietary stuff if it exists
+$(call inherit-product-if-exists, vendor/welcome/S25_mini/S25_mini-vendor.mk)
 
 # Inherit from S25_mini device
 $(call inherit-product, device/welcome/S25_mini/device.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6739:/system/etc/fstab.mt6739
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage \
+#    $(LOCAL_PATH)/overlay
+
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ACCDET.kl:system/usr/keylayout/ACCDET.kl \
+    $(LOCAL_PATH)/keylayout/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl
 
 PRODUCT_DEVICE := S25_mini
 PRODUCT_NAME := lineage_S25_mini
