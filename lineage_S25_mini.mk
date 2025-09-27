a4b71d9a67fd3bd527f90aa7a71b854f3813f9ea
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-09-27 23:24:16 +0100

    Or do we...

--- lineage_S25_mini.mk
+++ lineage_S25_mini.mk
@@ -25,8 +25,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/recovery.fstab:/system/etc/recovery.fstab
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/rootdir/etc/recovery.fstab:/system/etc/recovery.fstab
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
