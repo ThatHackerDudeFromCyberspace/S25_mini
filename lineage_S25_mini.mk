72d85951864a5510216d901b270c5ed114a30e12
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-09-27 18:08:46 +0100

    Fix inherit stuff???

--- lineage_S25_mini.mk
+++ lineage_S25_mini.mk
@@ -6,11 +6,10 @@
 #
 
 # Inherit from those products. Most specific first.
-include $(SRC_TARGET_DIR)/vendor/lineage/config/common_full_phone.mk
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit proprietary stuff if it exists
 $(call inherit-product-if-exists, vendor/welcome/S25_mini/S25_mini-vendor.mk)
