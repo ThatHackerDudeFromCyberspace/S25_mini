845001c194170652ae7b5b6f68f1f90bc0923918
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-09-29 12:30:31 +0100

    WHAT???

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 LOCAL_PATH := device/welcome/S25_mini
 
 # Set shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_SHIPPING_API_LEVEL := 28
 
 # Setup dalvik vm configs
 # Same as phone-xhdpi-6144-dalvik-heap.mk 
