654477085c93cb20d37cd0ab124dce5ed0ddde78
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-09-30 09:51:41 +0100

    Oops copied too much lol

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,13 +38,10 @@ TARGET_SCREEN_HEIGHT := 854
 TARGET_SCREEN_DENSITY := 240
 
 
-# HIDL
+# HIDL (Taken from stock)
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-TARGET_FS_CONFIG_GEN += \
-    $(DEVICE_PATH)/config.fs \
-    $(DEVICE_PATH)/mot_aids.fs
 
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 2
