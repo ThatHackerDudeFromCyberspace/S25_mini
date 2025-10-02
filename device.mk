ef4465e11ab55d5984267f6f7fd723fd8c64be18
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-10-02 10:48:44 +0100

    Add missing graphics services/impl

--- device.mk
+++ device.mk
@@ -96,10 +96,13 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json #@HD - TODO
 
 
-# Display
+# Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack-service.mediatek-mali
 
 # Stagefright
