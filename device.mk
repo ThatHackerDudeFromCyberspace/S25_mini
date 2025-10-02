2ee96e6fb79e6d9f6f4bd22b2cbcfd67ce81f0bd
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-10-02 13:36:35 +0100

    Add vendor boot thingie

--- device.mk
+++ device.mk
@@ -37,6 +37,13 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage \
 #    $(LOCAL_PATH)/overlay
 
+# Boot
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-impl.vendor \
+    android.hardware.boot@1.0-service
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ACCDET.kl:system/usr/keylayout/ACCDET.kl \
