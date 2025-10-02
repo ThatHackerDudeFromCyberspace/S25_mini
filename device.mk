e07be19fcac1a46d388d051f5e34b5d74eb2f87f
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-10-02 15:48:30 +0100

    Oops

--- device.mk
+++ device.mk
@@ -218,7 +218,7 @@ PRODUCT_PACKAGES += \
 
 # Memtrack
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
 # Vibrator
