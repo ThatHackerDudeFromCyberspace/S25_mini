1c00c443311b0f088369fb6e26fa1d151c1e9009
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-10-03 09:11:01 +0100

    Add keymaster proprietary files

--- device.mk
+++ device.mk
@@ -266,7 +266,6 @@ PRODUCT_PACKAGES += \
 
 # Keystore
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0.vendor \
     android.hardware.keymaster@4.0 \
     android.hardware.keymaster@4.0-impl \
     android.hardware.keymaster@4.0-service
