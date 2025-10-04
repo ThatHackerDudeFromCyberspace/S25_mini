5e7707782db1599c8c802c2904e7660e8f197430
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-10-04 17:07:59 +0100

    Cleanup audio

--- device.mk
+++ device.mk
@@ -59,6 +59,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@5.0-util.vendor \
     android.hardware.audio.common@5.0.vendor \
     android.hardware.audio@5.0.vendor \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0.vendor \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
@@ -66,6 +67,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.a2dp@1.0-service \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     libalsautils \
     libalsautils.vendor \
     libnbaio_mono \
